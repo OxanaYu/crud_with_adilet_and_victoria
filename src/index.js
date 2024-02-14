125435a813ce99b44c2cf21b2d204edef70e53be
Oxana <yurchenko.o@gmail.com>
2024-02-14 21:26:38 +0600

    add navbar

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import PostContext from "./components/context/PostContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <PostContext>
+      <App />
+    </PostContext>
   </BrowserRouter>
 );
