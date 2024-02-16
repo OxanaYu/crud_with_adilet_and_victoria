710ca9222835382d0a93029a210d97ab1e8d6447
Adilet Baimbekov <carpediem007700@gmail.com>
2024-02-16 15:57:39 +0600

    added pag

--- src/components/posts/PostList.jsx
+++ src/components/posts/PostList.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { Box } from "@mui/material";
+import React, { useState } from "react";
 
 const PostList = () => {
-  return <div>PostList</div>;
+  // !SEARCH
+
+  // const { getProducts, products } = useProducts();
+  // const [searchParams, setSearchParams] = useSearchParams();
+
+  // useEffect(() => {
+  //   getProducts();
+  // }, [searchParams]);
+  // console.log(products);
+
+  // !PAGINATION
+  const [page, setPage] = useState(1);
+  const itemPerPage = 4;
+  const count = Math.ceil(products.length / itemPerPage);
+
+  const currentData = () => {
+    const begin = (page - 1) * itemPerPage;
+    const end = begin + itemPerPage;
+    return products.slice(begin, end);
+  };
+  const handleChange = (e, value) => {
+    setPage(value);
+  };
+  return (
+    <div>
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4,1fr)",
+          gap: "30px",
+          flexWrap: "wrap",
+        }}
+      >
+        {currentData().map((elem) => (
+          <PostCard key={elem.id} elem={elem} />
+        ))}
+      </Box>
+      <PaginationControlled
+        count={count}
+        page={page}
+        handleChange={handleChange}
+      />
+    </div>
+  );
 };
 
 export default PostList;
