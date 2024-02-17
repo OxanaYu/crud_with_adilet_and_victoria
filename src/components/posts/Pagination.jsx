17d6e9b317760e61b66bcda365ccf16b5a30a5cc
Adilet Baimbekov <carpediem007700@gmail.com>
2024-02-17 21:36:39 +0600

    navbar

--- src/components/posts/Pagination.jsx
+++ src/components/posts/Pagination.jsx
@@ -4,9 +4,6 @@ import React from "react";
 const PaginationControlled = ({ count, page, handleChange }) => {
   return (
     <Stack spacing={2}>
-      count={count}
-      variant="outlined" color="secondary" onChange={handleChange}
-      =======
       <Typography>Page: {page} </Typography>
       <Pagination count={count} color="primary" onChange={handleChange} />
     </Stack>
